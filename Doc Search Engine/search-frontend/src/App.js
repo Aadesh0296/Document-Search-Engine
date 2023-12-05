a665e6e84d2e5e440e9e3e87c64ba00fc3b21c3f
Aadesh Sachin Lawange <114053824+Aadesh0296@users.noreply.github.com>
2023-12-05 17:06:21 +0530

    Update App.js

--- Doc Search Engine/search-frontend/src/App.js
+++ Doc Search Engine/search-frontend/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
-import UploadComponent from './FileUpload'
-import SearchComponent from './SearchComponent'
+import UploadComponent from './FileUpload';
+import SearchComponent from './SearchComponent';
+
 function App() {
   return (
-  <div className="App">
-    <div className="header">
-      <h1>
-        Document Search Engine
-      </h1>
+    <div className="App">
+      <div className="header">
+        <h1>Document Search Engine</h1>
+      </div>
+      <UploadComponent /> 
+      <SearchComponent /> 
+      <footer className="footer">
+        <p>
+          Designed and Developed by Work With Aadesh | 
+          Visit us at <a href="https://workwithaadesh.wordpress.com">Work With Aadesh</a> | 
+          Email: <a href="mailto:workwithaadesh@gmail.com">workwithaadesh@gmail.com</a> | 
+        </p>
+      </footer>
     </div>
-    <UploadComponent/> 
-    <SearchComponent/> 
- </div>
   );
 }
 
